0d17244bf2838e53d221b6db2d31266d2daa64cd
alper111 <ahmetoglu.alper@gmail.com>
2023-02-27 14:53:45 +0300

    new hw1

--- src/homework1.py
+++ src/homework1.py
@@ -1,106 +1,91 @@
-import time
+from multiprocessing import Process
 
 import numpy as np
+import torch
+import torchvision.transforms as transforms
 
 import environment
 
 
-class Homework1(environment.BaseEnv):
-    def __init__(self, n_actions=8) -> None:
-        super().__init__()
-        # divide the action space into n_actions
-        self._n_actions = n_actions
-        self._delta = 0.05
-
-        theta = np.linspace(0, 2*np.pi, n_actions)
-        actions = np.stack([np.cos(theta), np.sin(theta)], axis=1)
-        self._actions = {i: action for i, action in enumerate(actions)}
-
-        self._goal_thresh = 0.01
-        self._max_timesteps = 200
+class Hw1Env(environment.BaseEnv):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def _create_scene(self, seed=None):
         if seed is not None:
             np.random.seed(seed)
         scene = environment.create_tabletop_scene()
-        obj_pos = [np.random.uniform(0.25, 0.75),
-                   np.random.uniform(-0.3, 0.3),
-                   1.5]
-        goal_pos = [np.random.uniform(0.25, 0.75),
-                    np.random.uniform(-0.3, 0.3),
-                    1.025]
-        environment.create_object(scene, "capsule", pos=obj_pos, quat=[0, 0, 0, 1],
-                                  size=[0.02, 0.03], rgba=[0.8, 0.2, 0.2, 1],
-                                  name="obj1")
-        environment.create_visual(scene, "cylinder", pos=goal_pos, quat=[0, 0, 0, 1],
-                                  size=[0.05, 0.005], rgba=[0.2, 1.0, 0.2, 1],
-                                  name="goal")
+        r = np.random.rand()
+        if r < 0.5:
+            size = np.random.uniform([0.02, 0.02, 0.02], [0.03, 0.03, 0.03])
+            environment.create_object(scene, "box", pos=[0.6, 0., 1.1], quat=[0, 0, 0, 1],
+                                      size=size, rgba=[0.8, 0.2, 0.2, 1], friction=[0.02, 0.005, 0.0001],
+                                      density=4000, name="obj1")
+        else:
+            size = np.random.uniform([0.02, 0.02, 0.02], [0.03, 0.03, 0.03])
+            environment.create_object(scene, "sphere", pos=[0.6, 0., 1.1], quat=[0, 0, 0, 1],
+                                      size=size, rgba=[0.8, 0.2, 0.2, 1], friction=[0.2, 0.005, 0.0001],
+                                      density=4000, name="obj1")
         return scene
 
     def state(self):
-        ee_pos = self.data.site(self._ee_site).xpos[:2]
         obj_pos = self.data.body("obj1").xpos[:2]
-        goal_pos = self.data.site("goal").xpos[:2]
-        return np.concatenate([ee_pos, obj_pos, goal_pos])
+        pixels = self.viewer.read_pixels(camid=1).copy()
+        pixels = torch.tensor(pixels, dtype=torch.uint8).permute(2, 0, 1)
+        pixels = transforms.functional.center_crop(pixels, min(pixels.shape[1:]))
+        pixels = transforms.functional.resize(pixels, (128, 128))
+        return obj_pos, pixels
 
-    def reward(self):
-        state = self.state()
-        ee_pos = state[:2]
-        obj_pos = state[2:4]
-        goal_pos = state[4:6]
-        ee_to_obj = max(100*np.linalg.norm(ee_pos - obj_pos), 1)
-        obj_to_goal = max(100*np.linalg.norm(obj_pos - goal_pos), 1)
-        return 1/(ee_to_obj) + 1/(obj_to_goal)
+    def step(self, action_id):
+        if action_id == 0:
+            self._set_joint_position({6: 0.8})
+            self._set_ee_in_cartesian([0.4, 0, 1.065], rotation=[-90, 0, 180], n_splits=50)
+            self._set_ee_in_cartesian([0.8, 0, 1.065], rotation=[-90, 0, 180], n_splits=50)
+            self._set_ee_in_cartesian([0.4, 0, 1.065], rotation=[-90, 0, 180], n_splits=50)
+            self._set_joint_position({i: angle for i, angle in enumerate(self._init_position)})
+        elif action_id == 1:
+            self._set_joint_position({6: 0.8})
+            self._set_ee_in_cartesian([0.8, 0, 1.065], rotation=[-90, 0, 180], n_splits=50)
+            self._set_ee_in_cartesian([0.4, 0, 1.065], rotation=[-90, 0, 180], n_splits=50)
+            self._set_ee_in_cartesian([0.8, 0, 1.065], rotation=[-90, 0, 180], n_splits=50)
+            self._set_joint_position({i: angle for i, angle in enumerate(self._init_position)})
+        elif action_id == 2:
+            self._set_joint_position({6: 0.8})
+            self._set_ee_in_cartesian([0.6, -0.2, 1.065], rotation=[0, 0, 180], n_splits=50)
+            self._set_ee_in_cartesian([0.6, 0.2, 1.065], rotation=[0, 0, 180], n_splits=50)
+            self._set_ee_in_cartesian([0.6, -0.2, 1.065], rotation=[0, 0, 180], n_splits=50)
+            self._set_joint_position({i: angle for i, angle in enumerate(self._init_position)})
+        elif action_id == 3:
+            self._set_joint_position({6: 0.8})
+            self._set_ee_in_cartesian([0.6, 0.2, 1.065], rotation=[0, 0, 180], n_splits=50)
+            self._set_ee_in_cartesian([0.6, -0.2, 1.065], rotation=[0, 0, 180], n_splits=50)
+            self._set_ee_in_cartesian([0.6, 0.2, 1.065], rotation=[0, 0, 180], n_splits=50)
+            self._set_joint_position({i: angle for i, angle in enumerate(self._init_position)})
 
-    def is_terminal(self):
-        state = self.state()
-        obj_pos = state[2:4]
-        goal_pos = state[4:6]
-        return np.linalg.norm(obj_pos - goal_pos) < self._goal_thresh
 
-    def is_truncated(self):
-        return self._t >= self._max_timesteps
-
-    def step(self, action_id, render=None):
-        action = self._actions[action_id] * self._delta
-        ee_pos = self.data.site(self._ee_site).xpos[:2]
-        target_pos = np.concatenate([ee_pos, [1.06]])
-        target_pos[:2] = np.clip(target_pos[:2] + action, [0.25, -0.3], [0.75, 0.3])
-        if render is None:
-            self._set_ee_in_cartesian(target_pos, rotation=[-90, 0, 180], n_splits=30, threshold=0.04)
-        elif render == "step":
-            self._set_ee_in_cartesian(target_pos, rotation=[-90, 0, 180], n_splits=30, threshold=0.04)
-            self.render()
-        elif render == "full":
-            self._set_ee_in_cartesian(target_pos, rotation=[-90, 0, 180], n_splits=30, render=True, threshold=0.04)
-        self._t += 1
-
-        state = self.state()
-        reward = self.reward()
-        terminal = self.is_terminal()
-        truncated = self.is_truncated()
-        return state, reward, terminal, truncated
+def collect(idx, N):
+    env = Hw1Env(render_mode="offscreen")
+    positions = torch.zeros(N, 2, dtype=torch.float)
+    actions = torch.zeros(N, dtype=torch.uint8)
+    imgs = torch.zeros(N, 3, 128, 128, dtype=torch.uint8)
+    for i in range(N):
+        action_id = np.random.randint(4)
+        env.step(action_id)
+        obj_pos, pixels = env.state()
+        positions[i] = torch.tensor(obj_pos)
+        actions[i] = action_id
+        imgs[i] = pixels
+        env.reset()
+    torch.save(positions, f"positions_{idx}.pt")
+    torch.save(actions, f"actions_{idx}.pt")
+    torch.save(imgs, f"imgs_{idx}.pt")
 
 
 if __name__ == "__main__":
-    N_ACTIONS = 8
-    env = Homework1(n_actions=N_ACTIONS)
-    for episode in range(10):
-        done = False
-        cum_reward = 0.0
-        start = time.time()
-        while not done:
-            action = np.random.randint(N_ACTIONS)
-            state, reward, is_terminal, is_truncated = env.step(action, render="step")
-            done = is_terminal or is_truncated
-            cum_reward += reward
-        end = time.time()
-        print(f"Episode={episode}, reward={cum_reward:.2f}, RF={env.data.time/(end-start):.2f}")
-        env.reset()
-
-    # render a single episode
-    done = False
-    while not done:
-        action = np.random.randint(N_ACTIONS)
-        state, reward, is_terminal, is_truncated = env.step(action, render="full")
-        done = is_terminal or is_truncated
+    processes = []
+    for i in range(4):
+        p = Process(target=collect, args=(i, 100))
+        p.start()
+        processes.append(p)
+    for p in processes:
+        p.join()
