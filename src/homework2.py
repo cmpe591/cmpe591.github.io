392c8d7de0b97f3197a005d34af49caedd267dcd
alper111 <ahmetoglu.alper@gmail.com>
2023-02-27 14:53:57 +0300

    hw2 draft

---
+++ src/homework2.py
@@ -0,0 +1,101 @@
+import time
+
+import numpy as np
+
+import environment
+
+
+class Homework2(environment.BaseEnv):
+    def __init__(self, n_actions=8, **kwargs) -> None:
+        super().__init__(**kwargs)
+        # divide the action space into n_actions
+        self._n_actions = n_actions
+        self._delta = 0.05
+
+        theta = np.linspace(0, 2*np.pi, n_actions)
+        actions = np.stack([np.cos(theta), np.sin(theta)], axis=1)
+        self._actions = {i: action for i, action in enumerate(actions)}
+
+        self._goal_thresh = 0.01
+        self._max_timesteps = 200
+
+    def _create_scene(self, seed=None):
+        if seed is not None:
+            np.random.seed(seed)
+        scene = environment.create_tabletop_scene()
+        scene.option.timestep = 0.0075
+        obj_pos = [np.random.uniform(0.25, 0.75),
+                   np.random.uniform(-0.3, 0.3),
+                   1.5]
+        goal_pos = [np.random.uniform(0.25, 0.75),
+                    np.random.uniform(-0.3, 0.3),
+                    1.025]
+        environment.create_object(scene, "capsule", pos=obj_pos, quat=[0, 0, 0, 1],
+                                  size=[0.02, 0.03], rgba=[0.8, 0.2, 0.2, 1],
+                                  name="obj1")
+        environment.create_visual(scene, "cylinder", pos=goal_pos, quat=[0, 0, 0, 1],
+                                  size=[0.05, 0.005], rgba=[0.2, 1.0, 0.2, 1],
+                                  name="goal")
+        return scene
+
+    def state(self):
+        ee_pos = self.data.site(self._ee_site).xpos[:2]
+        obj_pos = self.data.body("obj1").xpos[:2]
+        goal_pos = self.data.site("goal").xpos[:2]
+        return np.concatenate([ee_pos, obj_pos, goal_pos])
+
+    def reward(self):
+        state = self.state()
+        ee_pos = state[:2]
+        obj_pos = state[2:4]
+        goal_pos = state[4:6]
+        ee_to_obj = max(100*np.linalg.norm(ee_pos - obj_pos), 1)
+        obj_to_goal = max(100*np.linalg.norm(obj_pos - goal_pos), 1)
+        return 1/(ee_to_obj) + 1/(obj_to_goal)
+
+    def is_terminal(self):
+        state = self.state()
+        obj_pos = state[2:4]
+        goal_pos = state[4:6]
+        return np.linalg.norm(obj_pos - goal_pos) < self._goal_thresh
+
+    def is_truncated(self):
+        return self._t >= self._max_timesteps
+
+    def step(self, action_id):
+        action = self._actions[action_id] * self._delta
+        ee_pos = self.data.site(self._ee_site).xpos[:2]
+        target_pos = np.concatenate([ee_pos, [1.06]])
+        target_pos[:2] = np.clip(target_pos[:2] + action, [0.25, -0.3], [0.75, 0.3])
+        self._set_ee_in_cartesian(target_pos, rotation=[-90, 0, 180], n_splits=30, threshold=0.04)
+        self._t += 1
+
+        state = self.state()
+        reward = self.reward()
+        terminal = self.is_terminal()
+        truncated = self.is_truncated()
+        return state, reward, terminal, truncated
+
+
+if __name__ == "__main__":
+    N_ACTIONS = 8
+    env = Homework2(n_actions=N_ACTIONS, render_mode="gui")
+    for episode in range(10):
+        done = False
+        cum_reward = 0.0
+        start = time.time()
+        while not done:
+            action = np.random.randint(N_ACTIONS)
+            state, reward, is_terminal, is_truncated = env.step(action)
+            done = is_terminal or is_truncated
+            cum_reward += reward
+        end = time.time()
+        print(f"Episode={episode}, reward={cum_reward:.2f}, RF={env.data.time/(end-start):.2f}")
+        env.reset()
+
+    # render a single episode
+    done = False
+    while not done:
+        action = np.random.randint(N_ACTIONS)
+        state, reward, is_terminal, is_truncated = env.step(action, render="full")
+        done = is_terminal or is_truncated
